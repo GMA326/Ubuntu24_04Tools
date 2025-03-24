7cd5acd385f1cc90e38fd0463d384755a4557ed2
GMA326 <g.manduca@gmx.de>
2025-03-24 15:06:42 +0100

    dockerinstall.sh

---
+++ dockerinstall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Let's install docker!"
+
+sudo apt update
+sudo apt install apt-transport-https curl
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo systemctl is-active docker
+
+sudo docker run hello-world
+
+sudo usermod -aG docker ${USER}
+
+sudo docker ps
